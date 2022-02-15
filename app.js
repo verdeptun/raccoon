80cb734f3e075303bedc7da759af75648f21d39f
verdeptun <verdeptun@gmail.com>
2022-02-15 23:44:51 +0000

    agregue otro tipo de extensiones

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 let pronoun = ['the','our'];
 let adj = ['great', 'big' ];
 let noun = ['jogger','racoon'];
+let url = ['.com', '.es', '.us', '.io', 'net'];
 for (i=0;i<2;i++) {
     for (j=0;j<2;j++) {
          for (k=0;k<2;k++){
-             console.log(pronoun[i]+adj[j]+noun[k]+".com") 
+             for (l=0;l<5;l++){
+             console.log(pronoun[i]+adj[j]+noun[k]+url[l]) 
          }
          
  }
-
+    }
     
 }
