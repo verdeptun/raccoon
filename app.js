c2ba4f91d528c1e9fa9bafebd2d04707ab098e4b
verdeptun <verdeptun@gmail.com>
2022-02-15 23:39:39 +0000

    tarea finalizada

---
+++ app.js
@@ -0,0 +1,13 @@
+let pronoun = ['the','our'];
+let adj = ['great', 'big' ];
+let noun = ['jogger','racoon'];
+for (i=0;i<2;i++) {
+    for (j=0;j<2;j++) {
+         for (k=0;k<2;k++){
+             console.log(pronoun[i]+adj[j]+noun[k]+".com") 
+         }
+         
+ }
+
+    
+}
